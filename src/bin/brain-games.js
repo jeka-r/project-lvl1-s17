b9cb972034e5ad90a77039dbedfd8e008780dba0
Evgeniy Radzivonchik <evgeniy.rad@gmail.com>
2017-01-18 21:04:57 +0300

    changes and adds for step 3 of project

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 import readlineSync from 'readline-sync';
 
-console.log ('Welcome to the Brain Games!');
+console.log('Welcome to the Brain Games!');
 const actual = readlineSync.question('May I have your name?: ');
-console.log (`Hello, ${actual}!`);
+console.log(`Hello, ${actual}!`);
